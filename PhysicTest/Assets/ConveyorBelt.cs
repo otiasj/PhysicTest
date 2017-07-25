8974f6c67fd9b5845c0e4e0422fadad387c9ead2
Julien Saito <julien.saito@gmail.com>
2017-07-24 22:11:27 -0700

    initial commit

---
+++ PhysicTest/Assets/ConveyorBelt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+
+    public float speed = 5f;
+    private Vector2 offset = new Vector2(0f, 0f);
+
+    public Renderer renderer;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        float offset = Time.time * speed;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+
+    void OnCollisionStay(Collision collided)
+    {
+        Debug.Log(GetComponent<Collider>().name + "<->" + collided.rigidbody.name);
+        Debug.DrawRay(collided.transform.position, transform.right * speed, Color.white);
+        //collided.rigidbody.MovePosition(collided.transform.position + transform.right * Time.deltaTime * speed);
+        collided.rigidbody.AddForce(transform.right * speed, ForceMode.Impulse);
+            //AddForce(collided.transform.position + transform.right * Time.deltaTime * speed);
+    }
+
+}
---
+++ PhysicTest/Assets/Fan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+    public int degreesPerSecond;
+    public int blowingForce;
+    private Vector3 force;
+
+    void Update () {
+        transform.Rotate(0, 0, degreesPerSecond * Time.deltaTime);
+        force = -this.transform.forward * blowingForce;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log(GetComponent<Collider>().name + "<->" + other.name);
+        //Debug.DrawRay(other.attachedRigidbody.position, force, Color.white);
+        other.attachedRigidbody.AddForceAtPosition(force, other.attachedRigidbody.position);
+    }
+
+}
