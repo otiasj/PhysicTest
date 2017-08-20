3b26615a901739d95b87e15e600b31c54617d3c7
Julien Saito <julien.saito@gmail.com>
2017-08-20 16:00:27 -0700

    explosions

---
+++ PhysicTest/Assets/Explosions/ExplosiveCube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveCube : MonoBehaviour
+{
+    public float force;
+    public float radius;
+    public float effectsRadius;
+
+    public GameObject explosion;
+
+    private void OnMouseDown()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider collided in colliders)
+        {
+            Rigidbody collidedRigidBody = collided.GetComponent<Rigidbody>();
+
+            if (collidedRigidBody != null)
+            {
+                collidedRigidBody.AddExplosionForce(force, transform.position, radius, .5f, ForceMode.Impulse);
+            }
+        }
+
+        ParticleSystem[] particleSystems = explosion.GetComponentsInChildren<ParticleSystem>();
+        for (int i = 0; i < particleSystems.Length; i++) {
+            ParticleSystem.ShapeModule shapeModule = particleSystems[i].shape;
+            shapeModule.radius = effectsRadius;
+        }
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
